5c732bf99a02a5c9f7f94b371a868de38c7f5771
Adam Schønemann <adamschoenemann@gmail.com>
2014-07-21 16:54:28 +0200

    adaptations and abstractions for Android

--- beat-planner/src/BeatPlanner.cs
+++ beat-planner/src/BeatPlanner.cs
@@ -5,280 +5,278 @@ using System.Text;
 
 namespace BeatPlanner
 {
-	public interface ITempo
-	{
-		float Tempo { get; }
-	}
+    public interface ITempo
+    {
+        float Tempo { get; }
+    }
 
-	public interface IBar
-	{
-		IMeter Meter { get; }
-	}
+    public interface IBar
+    {
+        IMeter Meter { get; }
+    }
 
-	public interface IMeter
-	{
-		int Lower { get; }
+    public interface IMeter
+    {
+        int Lower { get; }
 
-		int Upper { get; }
-	}
+        int Upper { get; }
+    }
 
-	public class Meter
-	{
-		public readonly int Upper;
-		public readonly int Lower;
+    public class Meter
+    {
+        public readonly int Upper;
+        public readonly int Lower;
 
-		public Meter(int u, int l)
-		{
-			Upper = u;
-			if (l == 2 || l == 4 || l == 8 || l == 16 || l == 32)
-				Lower = l;
-			else
-				throw new ArgumentException("Invalid lower meter given");
-		}
+        public Meter(int u, int l)
+        {
+            Upper = u;
+            if (l == 2 || l == 4 || l == 8 || l == 16 || l == 32)
+                Lower = l;
+            else
+                throw new ArgumentException("Invalid lower meter given");
+        }
 
-		public override string ToString()
-		{
-			return string.Format("{0}/{1}", Upper, Lower);
-		}
+        public override string ToString()
+        {
+            return string.Format("{0}/{1}", Upper, Lower);
+        }
 
-		public static readonly Meter Common = new Meter(4, 4);
-	}
+        public static readonly Meter Common = new Meter(4, 4);
+    }
 
-	public class Beat
-	{
-		public int BPM;
-		public Meter Meter;
+    public class Beat
+    {
+        public int BPM;
+        public Meter Meter;
 
-		public Beat(Meter meter, int BPM)
-		{
-			this.BPM = BPM;
-			this.Meter = meter;
-		}
+        public Beat(Meter meter, int BPM)
+        {
+            this.BPM = BPM;
+            this.Meter = meter;
+        }
 
-		public static Beat Parse(string[] splits)
-		{
-			string bpmStr = splits[1];
-			int bpm = Convert.ToInt32(bpmStr);
+        public static Beat Parse(string[] splits)
+        {
+            string bpmStr = splits[1];
+            int bpm = Convert.ToInt32(bpmStr);
 
-			string meterStr = splits[0];
-			string[] meterSplits = meterStr.Split('/');
-			int upper = Convert.ToInt32(meterSplits[0]);
-			int lower = Convert.ToInt32((meterSplits[1]));
+            string meterStr = splits[0];
+            string[] meterSplits = meterStr.Split('/');
+            int upper = Convert.ToInt32(meterSplits[0]);
+            int lower = Convert.ToInt32((meterSplits[1]));
 
-			return new Beat(new Meter(upper, lower), bpm);
-		}
-		// METER BPM
-		// 4/4   180
-		public static Beat Parse(string line)
-		{
-			string[] splits = line.Split(new [] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-			return Parse(splits);
+            return new Beat(new Meter(upper, lower), bpm);
+        }
+        // METER BPM
+        // 4/4   180
+        public static Beat Parse(string line)
+        {
+            string[] splits = line.Split(new [] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            return Parse(splits);
 
-		}
-	}
+        }
+    }
 
-	public class Track
-	{
-		private readonly List<Tuple<Beat, int>> sequences = new List<Tuple<Beat, int>>();
+    public class Track
+    {
+        private readonly List<Tuple<Beat, int>> sequences = new List<Tuple<Beat, int>>();
 
-		public Tuple<Beat, int>[] Sequences
-		{
-			get
-			{
-				Tuple<Beat,int>[] result = new Tuple<Beat, int>[sequences.Count];
-				sequences.CopyTo(result);
-				return result;
-			}
-		}
+        public Tuple<Beat, int>[] Sequences
+        {
+            get
+            {
+                Tuple<Beat,int>[] result = new Tuple<Beat, int>[sequences.Count];
+                sequences.CopyTo(result);
+                return result;
+            }
+        }
 
-		public void AppendSequence(Beat beat, int reps)
-		{
-			sequences.Add(new Tuple<Beat, int>(beat, reps));
-		}
+        public void AppendSequence(Beat beat, int reps)
+        {
+            sequences.Add(new Tuple<Beat, int>(beat, reps));
+        }
 
-		public void PrependSequence(Beat beat, int reps)
-		{
-			sequences.Insert(0, new Tuple<Beat, int>(beat, reps));
-		}
+        public void PrependSequence(Beat beat, int reps)
+        {
+            sequences.Insert(0, new Tuple<Beat, int>(beat, reps));
+        }
 
-		public void InsertSequence(Beat Beat, int reps, int i)
-		{
-			sequences.Insert(i, new Tuple<Beat, int>(Beat, reps));
-		}
-		// METER BPM REPS
-		// 4/4   180 4
-		private static Tuple<Beat, int> ParseSequence(string line)
-		{
-			string[] splits = line.Split(new []{ ' ' }, StringSplitOptions.RemoveEmptyEntries);
-			Beat beat = Beat.Parse(splits);
-			int reps = Convert.ToInt32(splits[2]);
-			return new Tuple<Beat, int>(beat, reps);
-		}
+        public void InsertSequence(Beat Beat, int reps, int i)
+        {
+            sequences.Insert(i, new Tuple<Beat, int>(Beat, reps));
+        }
+        // METER BPM REPS
+        // 4/4   180 4
+        private static Tuple<Beat, int> ParseSequence(string line)
+        {
+            string[] splits = line.Split(new []{ ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            Beat beat = Beat.Parse(splits);
+            int reps = Convert.ToInt32(splits[2]);
+            return new Tuple<Beat, int>(beat, reps);
+        }
 
-		public static Track Parse(string[] lines)
-		{
-			Track track = new Track();
-			Tuple<Beat, int> seq;
-			for (int i = 0; i < lines.Length; i++)
-			{
-				seq = ParseSequence(lines[i]);
-				track.AppendSequence(seq.Item1, seq.Item2);
-			}
-			return track;
-		}
+        public static Track Parse(string[] lines)
+        {
+            Track track = new Track();
+            Tuple<Beat, int> seq;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                seq = ParseSequence(lines[i]);
+                track.AppendSequence(seq.Item1, seq.Item2);
+            }
+            return track;
+        }
 
-		public static Track Parse(string input)
-		{
-			return Parse(input.Split(new []{ '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries));
-		}
+        public static Track Parse(string input)
+        {
+            return Parse(input.Split(new []{ '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries));
+        }
 
-		public string Compose()
-		{
-			StringBuilder sb = new StringBuilder();
-			foreach (Tuple<Beat, int> seq in sequences)
-			{
-				sb.AppendFormat("{0} {1} {2}{3}", seq.Item1.Meter, seq.Item1.BPM, 
-					seq.Item2, Environment.NewLine);
-			}
-			return sb.ToString();
-		}
+        public string Compose()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (Tuple<Beat, int> seq in sequences)
+            {
+                sb.AppendFormat("{0} {1} {2}{3}", seq.Item1.Meter, seq.Item1.BPM, 
+                    seq.Item2, Environment.NewLine);
+            }
+            return sb.ToString();
+        }
 
-		public void Save(string path)
-		{
-			File.WriteAllText(path, Compose());
-		}
+        public void Save(string path)
+        {
+            File.WriteAllText(path, Compose());
+        }
 
-		public static Track Load(string path)
-		{
-			string[] lines = File.ReadAllLines(path);
-			return Parse(lines);
-		}
-	}
+        public static Track Load(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+            return Parse(lines);
+        }
+    }
 
-	public class BeatPlanner
-	{
-		public static void Main(string[] args)
-		{
-			Track track = Track.Load("assets/track1.txt");
-			Metronome metro = new Metronome(
-				                  new Beat(Meter.Common, 60),
-				                  new WavSound("assets/llfclave_low.wav"),
-				                  new WavSound("assets/llfclave.wav")
-			                  );
+    public class BeatPlanner
+    {
+        public static void Main(string[] args)
+        {
+            Track track = Track.Load("assets/track1.txt");
+            Metronome metro = new Metronome(
+                         new Beat(Meter.Common, 60)
+                     );
 
-			TrackPlayer player = new TrackPlayer(track, metro);
-			player.PlayAsync();
-			String read = "";
-			do
-			{
-				if (read == "stop")
-				{
-					player.Stop();
-				}
-				read = Console.ReadLine();
-			} while (read != "q");
-			player.Stop();
-		}
+            TrackPlayer player = new TrackPlayer(track, metro);
+            player.PlayAsync();
+            String read = "";
+            do
+            {
+                if (read == "stop")
+                {
+                    player.Stop();
+                }
+                read = Console.ReadLine();
+            } while (read != "q");
+            player.Stop();
+        }
 
-		public static void LoadSaveTest(string[] args)
-		{
-			Track track = Track.Load("assets/track1.txt");
-			TrackPlayer player = new TrackPlayer(track);
-			track.Save("assets/track2.txt");
+        public static void LoadSaveTest(string[] args)
+        {
+            Track track = Track.Load("assets/track1.txt");
+            TrackPlayer player = new TrackPlayer(track);
+            track.Save("assets/track2.txt");
 //			player.Play();
-		}
+        }
 
-		public static void ParseBeatTest(string[] args)
-		{
-			string input = Console.ReadLine();
-			Metronome metro = new Metronome();
+        public static void ParseBeatTest(string[] args)
+        {
+            string input = Console.ReadLine();
+            Metronome metro = new Metronome();
 
-			while (input != "q")
-			{
-				Beat beat = Beat.Parse(input);
-				metro.Beat = beat;
-				metro.Start();
-				while (metro.Bars < 2)
-					;
-				metro.Stop();
-				metro.Reset();
-				input = Console.ReadLine();
-			}
-		}
+            while (input != "q")
+            {
+                Beat beat = Beat.Parse(input);
+                metro.Beat = beat;
+                metro.Start();
+                while (metro.Bars < 2)
+                    ;
+                metro.Stop();
+                metro.Reset();
+                input = Console.ReadLine();
+            }
+        }
 
-		public static void TrackTest(string[] args)
-		{
-			Track track = new Track();
-			TrackPlayer player = new TrackPlayer(track);
-			track.AppendSequence(new Beat(Meter.Common, 80), 4);
-			track.AppendSequence(new Beat(new Meter(3, 4), 120), 4);
-			track.AppendSequence(new Beat(new Meter(7, 8), 200), 4);
-			track.PrependSequence(new Beat(new Meter(5, 4), 160), 2);
+        public static void TrackTest(string[] args)
+        {
+            Track track = new Track();
+            TrackPlayer player = new TrackPlayer(track);
+            track.AppendSequence(new Beat(Meter.Common, 80), 4);
+            track.AppendSequence(new Beat(new Meter(3, 4), 120), 4);
+            track.AppendSequence(new Beat(new Meter(7, 8), 200), 4);
+            track.PrependSequence(new Beat(new Meter(5, 4), 160), 2);
 
-			player.Play();
-		}
+            player.Play();
+        }
 
-		public static void MetroTest(string[] args)
-		{
-			Console.WriteLine("Hello!");
+        public static void MetroTest(string[] args)
+        {
+            Console.WriteLine("Hello!");
 
-			Metronome metro = new Metronome();
-			// metro.Start();
+            Metronome metro = new Metronome();
+            // metro.Start();
 			
 
-			String read;
-			while (true)
-			{
-				read = Console.ReadLine();
-				switch (read)
-				{
-					case "q":
-					case "quit":
-						metro.Stop();
-						return;
+            String read;
+            while (true)
+            {
+                read = Console.ReadLine();
+                switch (read)
+                {
+                    case "q":
+                    case "quit":
+                        metro.Stop();
+                        return;
 
-					case "stop":
-						metro.Stop();
-						break;
+                    case "stop":
+                        metro.Stop();
+                        break;
 
-					case "start":
-						metro.Start();
-						break;
+                    case "start":
+                        metro.Start();
+                        break;
 
-					case "reset":
-						metro.Reset();
-						break;
+                    case "reset":
+                        metro.Reset();
+                        break;
 
-					case "restart":
-						metro.Restart();
-						break;
+                    case "restart":
+                        metro.Restart();
+                        break;
 
-					case "bpm":
-						string bpmstr = Console.ReadLine();
-						int bpm = Convert.ToInt32(bpmstr);
-						metro.Lock.EnterWriteLock();
-						metro.BPM = bpm;
-						metro.Lock.ExitWriteLock();
+                    case "bpm":
+                        string bpmstr = Console.ReadLine();
+                        int bpm = Convert.ToInt32(bpmstr);
+                        metro.Lock.EnterWriteLock();
+                        metro.BPM = bpm;
+                        metro.Lock.ExitWriteLock();
 						// metro.BPM = bpm;
-						break;
+                        break;
 
-					case "beat":
-						string beatstr = Console.ReadLine(); // e.g. 3/4
-						string[] nums = beatstr.Split('/');
-						int upper = Convert.ToInt32(nums[0]);
-						int lower = Convert.ToInt32(nums[1]);
+                    case "beat":
+                        string beatstr = Console.ReadLine(); // e.g. 3/4
+                        string[] nums = beatstr.Split('/');
+                        int upper = Convert.ToInt32(nums[0]);
+                        int lower = Convert.ToInt32(nums[1]);
 						// Console.WriteLine(upper);
 						// Console.WriteLine(lower);
-						metro.Lock.EnterWriteLock();
-						metro.Meter = new Meter(upper, lower);
-						metro.Lock.ExitWriteLock();
-						break;
+                        metro.Lock.EnterWriteLock();
+                        metro.Meter = new Meter(upper, lower);
+                        metro.Lock.ExitWriteLock();
+                        break;
 
-					default:
-						break;
-				}
-			}
+                    default:
+                        break;
+                }
+            }
 			
-		}
-	}
+        }
+    }
 }
--- beat-planner/src/Metronome.cs
+++ beat-planner/src/Metronome.cs
@@ -1,188 +1,181 @@
 using System;
 using System.Diagnostics;
 using System.Threading;
-using System.Media;
+using Android.Media;
+using Android.Content.Res;
+
 
 namespace BeatPlanner
 {
-	public class BeatEventArgs : EventArgs
-	{
-		public readonly int Index, Beat, Bar;
+    public class BeatEventArgs : EventArgs
+    {
+        public readonly int Index, Beat, Bar;
 
-		public BeatEventArgs(int i, int b, int bar)
-		{
-			Index = i;
-			Beat = b;
-			Bar = bar;
-		}
-	}
+        public BeatEventArgs(int i, int b, int bar)
+        {
+            Index = i;
+            Beat = b;
+            Bar = bar;
+        }
+    }
 
-	public class Metronome
-	{
-		private class BeatEnumerator
-		{
-			public struct Info
-			{
-				public readonly int Index;
-				public readonly long Duration;
+    public class Metronome
+    {
+        private class BeatEnumerator
+        {
+            public struct Info
+            {
+                public readonly int Index;
+                public readonly long Duration;
 
-				public Info(int i, long d)
-				{
-					Index = i;
-					Duration = d;
-				}
-			}
+                public Info(int i, long d)
+                {
+                    Index = i;
+                    Duration = d;
+                }
+            }
 
-			public readonly Beat Beat;
-			private int index = 1;
+            public readonly Beat Beat;
+            private int index = 1;
 
-			public BeatEnumerator(Beat beat)
-			{
-				this.Beat = beat;
-			}
+            public BeatEnumerator(Beat beat)
+            {
+                this.Beat = beat;
+            }
 
-			public Info Next()
-			{
-				int retIndex = index;
-				index++;
-				if (index > Beat.Meter.Upper)
-					index = 1;
+            public Info Next()
+            {
+                int retIndex = index;
+                index++;
+                if (index > Beat.Meter.Upper)
+                    index = 1;
 
-				long fourthDur = 60000 / Beat.BPM;
-				long dur = (long)(fourthDur * (4 / (float)Beat.Meter.Lower));
+                long fourthDur = 60000 / Beat.BPM;
+                long dur = (long)(fourthDur * (4 / (float)Beat.Meter.Lower));
 
-				return new Info(retIndex, dur);
-			}
-		}
+                return new Info(retIndex, dur);
+            }
+        }
 
-		private int beats = 0;
+        private int beats = 0;
 
-		public int Beats { get { return beats; } private set { beats = value; } }
+        public int Beats { get { return beats; } private set { beats = value; } }
 
-		private int bars = 0;
+        private int bars = 0;
 
-		public int Bars { get { return bars; } private set { bars = value; } }
+        public int Bars { get { return bars; } private set { bars = value; } }
 
-		public Beat Beat
-		{
-			get { return bEnum.Beat; }
-			set
-			{
-				bEnum = new BeatEnumerator(value);
-			}
-		}
+        public Beat Beat
+        {
+            get { return bEnum.Beat; }
+            set
+            {
+                bEnum = new BeatEnumerator(value);
+            }
+        }
 
-		public int BPM
-		{
-			get { return Beat.BPM; }
-			set
-			{
-				Beat.BPM = value;
-			}
-		}
+        public int BPM
+        {
+            get { return Beat.BPM; }
+            set
+            {
+                Beat.BPM = value;
+            }
+        }
 
-		public Meter Meter
-		{
-			get { return Beat.Meter; }
-			set { Beat = new Beat(value, Beat.BPM); }
-		}
-		// Not really used
-		private Stopwatch sw;
-		private Thread thread;
-		private SoundPlayer accentSoundPlayer;
-		private SoundPlayer soundPlayer;
-		private BeatEnumerator bEnum;
-		private bool shouldStop = false;
-		public EventHandler<BeatEventArgs> BeatEvent;
+        public Meter Meter
+        {
+            get { return Beat.Meter; }
+            set { Beat = new Beat(value, Beat.BPM); }
+        }
+        // Not really used
+        private Stopwatch sw;
+        private Thread thread;
+ 
+        private BeatEnumerator bEnum;
+        private bool shouldStop = false;
+        public EventHandler<BeatEventArgs> BeatEvent;
 
-		public ReaderWriterLockSlim Lock { get; private set; }
+        public ReaderWriterLockSlim Lock { get; private set; }
 
-		public Metronome(Beat beat, ISound sound, ISound accSound)
-		{
-			sw = new Stopwatch();
-			bEnum = new BeatEnumerator(beat);
-			soundPlayer = new SoundPlayer(sound.Stream);
-			accentSoundPlayer = new SoundPlayer(accSound.Stream);
-			Beats = 0;
-			Lock = new ReaderWriterLockSlim();
-		}
+        public Metronome(Beat beat)
+        {
+            sw = new Stopwatch();
+            bEnum = new BeatEnumerator(beat);
+            Beats = 0;
+            Lock = new ReaderWriterLockSlim();
+        }
 
-		public Metronome() : 
-			this(new Beat(Meter.Common, 60), 
-			     new BeepSound(440, 50, ushort.MaxValue / 2), 
-			     new BeepSound(660, 50, ushort.MaxValue / 2))
-		{
+        public Metronome() :
+            this(new Beat(Meter.Common, 60))
+        {
 
-		}
+        }
 
-		public void Start()
-		{
-			if (thread != null && thread.IsAlive) // already running
+        public void Start()
+        {
+            if (thread != null && thread.IsAlive) // already running
 				return;
-			thread = new Thread(Loop);
-			thread.Priority = ThreadPriority.Highest;
-			sw.Start();
+            thread = new Thread(Loop);
+            thread.Priority = ThreadPriority.Highest;
+            sw.Start();
 
-			thread.Start();
-		}
+            thread.Start();
+        }
 
-		public void Stop()
-		{
-			if (thread != null && thread.IsAlive)
-			{
-				sw.Stop();
-				shouldStop = true;
-				thread.Join();
+        public void Stop()
+        {
+            if (thread != null && thread.IsAlive)
+            {
+                sw.Stop();
+                shouldStop = true;
+                thread.Join();
 
-				Console.WriteLine("Metronome stopped");
-			}
+                Console.WriteLine("Metronome stopped");
+            }
 				
-		}
+        }
 
-		public void Reset()
-		{
-			Beats = 0;
-			Bars = 0;
-			sw.Reset();
-			bEnum = new BeatEnumerator(bEnum.Beat);
-			if (thread.IsAlive)
-			{
-				sw.Start();
-			}
-		}
+        public void Reset()
+        {
+            Beats = 0;
+            Bars = 0;
+            sw.Reset();
+            bEnum = new BeatEnumerator(bEnum.Beat);
+            if (thread.IsAlive)
+            {
+                sw.Start();
+            }
+        }
 
-		public void Restart()
-		{
-			Reset();
-			if (thread.IsAlive == false)
-				Start();
+        public void Restart()
+        {
+            Reset();
+            if (thread.IsAlive == false)
+                Start();
 
-		}
+        }
 
-		private void Loop()
-		{
-			//Thread.Sleep(200); // HACK to get first beat audible
-			BeatEnumerator.Info info;
-			while (shouldStop == false)
-			{
-				Lock.EnterReadLock();
-				info = bEnum.Next();
-				Lock.ExitReadLock();
-				if (info.Index == Beat.Meter.Upper)
-					Interlocked.Increment(ref bars);
-				Interlocked.Increment(ref beats);
-				if (info.Index == 1)
-					accentSoundPlayer.Play();
-				else
-					soundPlayer.Play();
+        private void Loop()
+        {
+            //Thread.Sleep(200); // HACK to get first beat audible
+            BeatEnumerator.Info info;
+            while (shouldStop == false)
+            {
+                Lock.EnterReadLock();
+                info = bEnum.Next();
+                Lock.ExitReadLock();
+                if (info.Index == Beat.Meter.Upper)
+                    Interlocked.Increment(ref bars);
+                Interlocked.Increment(ref beats);
 
-				if (BeatEvent != null)
-					BeatEvent(this, new BeatEventArgs(info.Index, Beats, Bars));
+                if (BeatEvent != null)
+                    BeatEvent(this, new BeatEventArgs(info.Index, Beats, Bars));
 
-				Console.WriteLine(sw.ElapsedMilliseconds + ", Index: " + info.Index + ", Bars: " + Bars);
-				Thread.Sleep((int)info.Duration); // Not very accurate, unfortunately. Alternatives appear limited
-			}
-			shouldStop = false;
-		}
-	}
+                Console.WriteLine(sw.ElapsedMilliseconds + ", Index: " + info.Index + ", Bars: " + Bars);
+                Thread.Sleep((int)info.Duration); // Not very accurate, unfortunately. Alternatives appear limited
+            }
+            shouldStop = false;
+        }
+    }
 }
